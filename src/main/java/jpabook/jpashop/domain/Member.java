01e7e13975d52f93fbdce02bcff8ddec261487bb
zziminking <116048069+zziminking@users.noreply.github.com>
2023-12-03 16:51:47 +0900

    20231203

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address adderss;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new  ArrayList<>();
+
+}
